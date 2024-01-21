75a6735e58ae498a9800f614e2b312cd54f4ce7f
zazajococo <elsacoralie@hotmail.com>
2024-01-21 16:42:16 +0100

    modif index.html

--- main.js
+++ main.js
@@ -21,3 +21,79 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  const slides = document.getElementsByClassName("slide");
+  const dots = document.getElementsByClassName("dot");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+
+  setTimeout(showSlides, 2000); // Change slide every 2 seconds
+}
+
+function searchFunction() {
+  // Déclare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("searchBox");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("searchResults");
+  li = ul.getElementsByTagName("li");
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+
+const burgerMenu = document.querySelector(".burger-menu");
+const menu = document.querySelector(".menu");
+
+burgerMenu.addEventListener("click", () => {
+  burgerMenu.classList.toggle("open");
+  menu.classList.toggle("open");
+});
+
+function searchFunction() {
+  // Déclare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("searchBox");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("searchResults");
+  li = ul.getElementsByTagName("li");
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
