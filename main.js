0b78ca8c4faf1606c15ae0f20ef322c80083f998
Paul-Elouan <paulelouan.gl@gmail.com>
2024-01-21 15:12:37 +0100

    /

--- main.js
+++ main.js
@@ -12,3 +12,12 @@ themeToggleBtn.addEventListener("click", () => {
     localStorage.removeItem("theme");
   }
 });
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
